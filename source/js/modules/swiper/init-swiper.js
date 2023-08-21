d1e6a42564845b1a4bc64c332a8bf664707a3b93
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2023-08-22 01:00:50 +0300

    Добавляет блок Ближайшие туры

--- source/js/modules/swiper/init-swiper.js
+++ source/js/modules/swiper/init-swiper.js
@@ -1,4 +1,5 @@
 const swiperContainerHero = document.querySelector('.hero__container');
+const swiperContainerTours = document.querySelector('.tours__container');
 
 const initSwiper = () => {
   const swiperHero = new Swiper(swiperContainerHero, {
@@ -10,10 +11,31 @@ const initSwiper = () => {
       bulletActiveClass: 'hero__bullet--active',
       clickable: true,
     },
-    grabCursor: true,
     simulateTouch: false,
     loop: true,
     speed: 300,
+  });
+
+  const swiperTours = new Swiper(swiperContainerTours, {
+    navigation: {
+      nextEl: '.tours__button--next',
+      prevEl: '.tours__button--prev',
+    },
+    simulateTouch: false,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 18,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
   });
 };
 
